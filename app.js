84cc74b185ebab3e23797d4f7a29362581005fdb
F. Javier R. Donado <fjrd84@gmail.com>
2017-04-05 22:06:43 +0200

    Beanstalkd connection initialization.

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express'),
+  path = require('path'),
+  favicon = require('serve-favicon'),
+  logger = require('morgan'),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser'),
+  index = require('./routes/index'),
+  analysis = require('./routes/analysis'),
+  fivebeans = require('fivebeans'),
+  app = express();
 
-var index = require('./routes/index');
-var users = require('./routes/analysis');
+// Initialize the connection with beanstalkd
+const beanstalkd = new fivebeans.client('localhost', 11300);
 
-var app = express();
+beanstalkd
+  .on('connect', function () {
+    beanstalkd.use('default', function (err, name) {
+      console.log('Using beanstalkd.')
+    })
+  }).on('error', function (err) {
+    console.log('An error occurred...');
+  })
+  .on('close', function () {
+    console.log('...Closing the tube...');
+  })
+  .connect();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,17 +32,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/analysis', users);
+app.use('/analysis', analysis(beanstalkd));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
