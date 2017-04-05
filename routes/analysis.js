6a4312f47c0a4dae9c3056ad409d70bbab6554e0
F. Javier R. Donado <fjrd84@gmail.com>
2017-04-05 22:07:23 +0200

    Inject beanstalkd dependency.

--- routes/analysis.js
+++ routes/analysis.js
@@ -1,14 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express'),
+  router = express.Router();
 
-/* POST a new analysis. */
-router.post('/', function (req, res, next) {
-  let analysis = Object.assign({}, req.body);
-  analysis.created_at = (new Date()).toISOString();
-  res.json({
-    message: 'Analysis received',
-    data: analysis
-  })
-});
-
-module.exports = router;
+/**
+ * An active connection to beanstalkd must be injected in order
+ * to save the jobs in the jobs queue.
+ * @param {Object} beanstalkd Active connection to beanstalkd, fivebeans based.
+ */
+module.exports = (beanstalkd) => {
+  /* POST a new analysis. */
+  router.post('/', function (req, res, next) {
+    let analysis = Object.assign({}, req.body);
+    analysis.created_at = (new Date()).toISOString();
+    beanstalkd.put(0, 0, 60, JSON.stringify(analysis),
+      function (err, jobid) {
+        res.json({
+          message: 'Analysis received',
+          data: analysis
+        })
+      })
+  });
+  return router;
+};
