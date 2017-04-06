7c0bde3360a32d32b6bb3b8483018485039936ea
F. Javier R. Donado <fjrd84@gmail.com>
2017-04-06 22:17:10 +0200

    Return error in development.

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.json({ message: 'An error occurred.' });
+  res.json(res.locals.error);
 });
 
 module.exports = app;
