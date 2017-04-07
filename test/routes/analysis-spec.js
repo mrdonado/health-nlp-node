b832d2eaee2030de1ea8f08136c9f391380c3023
F. Javier R. Donado <fjrd84@gmail.com>
2017-04-07 22:59:50 +0200

    Analysis specs.

---
+++ test/routes/analysis-spec.js
@@ -0,0 +1,40 @@
+const mocha = require('mocha'),
+    mockRequire = require('mock-require');
+mockRequire.reRequire('../../app');
+const app = require('../../app'),
+    chai = require('chai'),
+    chaiHttp = require('chai-http'),
+    expect = chai.expect,
+    analysis = require('../../routes/analysis');
+
+
+
+describe('analysis routes', () => {
+    let jobSent = false;
+
+    app.use('/analysis', new analysis({
+        put: function (a, b, c, jsonString, cb) {
+            let json = JSON.parse(jsonString);
+            expect(json.message).to.equal('Some input message');
+            jobSent = true;
+            cb();
+        }
+    }));
+
+    it('should put the received job into the beanstalkd queue', (done) => {
+        let newJob = {
+            message: 'Some input message'
+        };
+        chai.request(app)
+            .post('/analysis')
+            .field('message', 'Some input message')
+            .end((err, res) => {
+                //{ message: 'Job received', data: newJob },
+                //expect(jobSent).to.equal(true);
+                //expect(res.body.message).to.equal('Job received');
+                expect(err).to.equal(null);
+                done();
+            });
+
+    });
+});
