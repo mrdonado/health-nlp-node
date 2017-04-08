825eae531c124ca0ea8ef75b836a310bd8f3bba8
F. Javier R. Donado <fjrd84@gmail.com>
2017-04-08 22:11:22 +0200

    Use chai-http.

--- test/routes/analysis-spec.js
+++ test/routes/analysis-spec.js
@@ -7,7 +7,7 @@ const app = require('../../app'),
     expect = chai.expect,
     analysis = require('../../routes/analysis');
 
-
+chai.use(chaiHttp);
 
 describe('analysis routes', () => {
     let jobSent = false;
@@ -31,7 +31,7 @@ describe('analysis routes', () => {
             .end((err, res) => {
                 //{ message: 'Job received', data: newJob },
                 //expect(jobSent).to.equal(true);
-                //expect(res.body.message).to.equal('Job received');
+                expect(res.body.message).to.equal('Job received');
                 expect(err).to.equal(null);
                 done();
             });
