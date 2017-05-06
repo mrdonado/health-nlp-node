cb0f105d796a7f72cae5114873e0476dcff86512
F. Javier R. Donado <fjrd84@gmail.com>
2017-05-06 19:55:23 +0200

    CORS configuration.

--- boot/configuration.js
+++ boot/configuration.js
@@ -7,6 +7,9 @@ let configuration = {
         host: process.env.BEANSTALKD_HOST || 'localhost',
         port: process.env.BEANSTALKD_PORT || 11300
     },
+    cors: {
+        allowedOrigin: process.env.ALLOWED_ORIGIN || 'http://localhost:4200'
+    },
     errorLevel: process.env.ERROR_LEVEL || 'trace'
 };
 
