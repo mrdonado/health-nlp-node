570e9d5cb5af66b8ca5dad4cf2abec25aeae9220
F. Javier R. Donado <fjrd84@gmail.com>
2017-05-18 22:28:15 +0200

    Twitter stream and spec.

--- boot/twitter-stream.js
+++ boot/twitter-stream.js
@@ -1,5 +1,8 @@
-const log = require('./logger');
-
+/**
+ * Given the twitter constructor (see module 'twitter') and the
+ * configuration parameters, it returns a twitter client object,
+ * that can be directly used to interact with the twitter API.
+ */
 const getTwitterClient = (Twitter, config) => {
   return new Twitter({
     consumer_key: config.twitter.consumerKey,
@@ -9,28 +12,60 @@ const getTwitterClient = (Twitter, config) => {
   });
 };
 
+/**
+ * The words coming from a file are turned into an array
+ * by splitting on every new line.
+ */
 const parseWords = (data) => {
   return data.toString().split('\n');
 };
 
+/**
+ * It obtains longest word of the words array that is present
+ * into the text.
+ * @param text:string
+ * @param words: string[]
+ * @returns word:string the longest matching word or an empty
+ * character if no match has been found.
+ */
 const induceQuery = (text, words) => {
   return words.reduce((q, word) => {
-    if (text.indexOf(word) > -1 && word.length > q) {
+    if (text.indexOf(word) > -1 && word.length > q.length) {
       return word;
     }
     return q;
   }, '');
 };
 
+/**
+ * Return a promise that resolves with the specified file's contents
+ * when it has been successfully loaded.
+ * 
+ * The promise will be rejected if an exception happened while trying
+ * to read the file.
+ */
 const parseFile = (fs, wordsFileName) => {
   return new Promise((resolve, reject) => {
     fs.readFile(wordsFileName, (err, data) => {
-      if (err) { return reject(err); }
+      if (err) { return reject(new Error(err)); }
       resolve(parseWords(data));
     });
   });
 };
 
+/**
+ * Callback for the twitter stream when data are coming in.
+ * 
+ * This is a high order function. First configure it with an instance
+ * of fivebeans' beanstalkd client and a list of words used as start
+ * words.
+ * 
+ * A function will be returned that will be used as event callback
+ * for incoming data.
+ * 
+ * The incoming data (message) will be sent to beanstalkd as a new
+ * job for the analyzer.
+ */
 const dataCb = (beanstalkd, words) => {
   return (event) => {
     const query = induceQuery(event.text, words);
@@ -45,33 +80,41 @@ const dataCb = (beanstalkd, words) => {
       'source': 'twitter',
       'query': query
     };
-    beanstalkd.put(10, 1, 60, JSON.stringify(job),
+    beanstalkd.put(10, 1, 1, JSON.stringify(job),
       (err, jobid) => {
         //
       });
   };
 };
 
-const errorCb = function (error) {
-  log.error('Twitter stream error:' + error.toString());
+/**
+ * High order function. First call it with your logger, then it returns
+ * a function that can be used as error callback for the twitter stream.
+ */
+const errorCb = (log) => {
+  return (error) => {
+    log.error('Twitter stream error:' + error.toString());
+  };
 };
 
-const startStream = (Twitter, beanstalkd, config) => {
+const startStream = (Twitter, beanstalkd, config, log) => {
   return (words) => {
     const client = getTwitterClient(Twitter, config);
     const stream = client.stream('statuses/filter', {
-      track: words.join(',')
+      track: words.join(','),
+      filter_level: 'low',
+      language: 'en'
     });
     stream.on('data', dataCb(beanstalkd, words));
-    stream.on('error', errorCb);
+    stream.on('error', errorCb(log));
   };
 };
 
 
 module.exports = {
-  runTwitterStream: (Twitter, beanstalkd, fs, config) => {
+  runTwitterStream: (Twitter, beanstalkd, fs, config, log) => {
     parseFile(fs, './boot/twitter-query-words.txt')
-      .then(startStream(Twitter, beanstalkd, config));
+      .then(startStream(Twitter, beanstalkd, config, log));
   },
   startStream,
   errorCb,
---
+++ test/boot/twitter-stream-spec.js
@@ -0,0 +1,140 @@
+const mocha = require('mocha'),
+  chai = require('chai'),
+  sinon = require('sinon'),
+  twitterStream = require('../../boot/twitter-stream'),
+  log4js = require('log4js'),
+  expect = chai.expect;
+
+
+describe('twitter stream components', () => {
+
+  it('should create the twitter client', () => {
+    const Twitter = sinon.stub();
+    const config = {
+      twitter: {
+        consumerKey: 'ck',
+        consumerSecret: 'cs',
+        accessTokenKey: 'atk',
+        accessTokenSecret: 'ats'
+      }
+    };
+    twitterStream.getTwitterClient(Twitter, config);
+    expect(Twitter.args[0][0]).to.eql({
+      consumer_key: 'ck',
+      consumer_secret: 'cs',
+      access_token_key: 'atk',
+      access_token_secret: 'ats'
+    });
+  });
+
+  it('should parse the words', () => {
+    const words = twitterStream.parseWords('word1\nword2');
+    expect(words).to.eql(['word1', 'word2'])
+  });
+
+  it('should induce a query', () => {
+    const query1 = twitterStream
+      .induceQuery('some example text',
+      ['word1', 'word2', 'example']);
+
+    expect(query1).to.eql('example');
+
+    const query2 = twitterStream
+      .induceQuery('some example text',
+      ['word1', 'text', 'example']);
+
+    expect(query2).to.eql('example');
+
+    const query3 = twitterStream
+      .induceQuery('some example text',
+      ['word1', 'word2', 'word3']);
+
+    expect(query3).to.eql('');
+
+  });
+
+  it('should parse a file', (done) => {
+    const fs = {
+      readFile: (filename, cb) => {
+        cb(null, 'some data');
+      }
+    }
+    twitterStream
+      .parseFile(fs, 'somefile.txt')
+      .then((data) => {
+        expect(data).to.deep.equal(['some data']);
+        done();
+      });
+  });
+
+  it('should insert a job into the beanstalkd', () => {
+    const beanstalkd = { put: sinon.stub() };
+    words = ['test1', 'test2', 'test3'],
+      demoEvent = {
+        text: 'Some test1 to rule the world',
+        lang: 'en',
+        user: {
+          screen_name: 'fakeuser4you',
+          description: 'this is a real fake user'
+        }
+      },
+      expectedJob = {
+        user_name: 'fakeuser4you',
+        user_description: 'this is a real fake user',
+        message: 'Some test1 to rule the world',
+        source: 'twitter',
+        query: 'test1'
+      };
+    twitterStream.dataCb(beanstalkd, words)(demoEvent);
+    expect(beanstalkd.put.args[0][0]).to.eql(10);
+    expect(beanstalkd.put.args[0][1]).to.eql(1);
+    expect(beanstalkd.put.args[0][2]).to.eql(1);
+    let receivedJob = JSON.parse(beanstalkd.put.args[0][3]);
+    delete receivedJob.created_at;
+    expect(receivedJob).to.eql(expectedJob);
+
+  });
+
+  it('should try to parse a file that doesn\'t exist', (done) => {
+    const fs = {
+      readFile: (filename, cb) => {
+        cb('not found', null);
+      }
+    }
+    twitterStream
+      .parseFile(fs, 'somefile.txt')
+      .then((data) => {
+        console.warn(data);
+      })
+      .catch((err) => {
+        expect(err.message).to.equal('not found');
+        done();
+      });
+  });
+
+  it('should log an error', () => {
+    const log = { error: sinon.stub() };
+    twitterStream.errorCb(log)('Some error');
+    expect(log.error.args[0]).to.match(/Twitter stream error/);
+  });
+
+  it('should start a stream', () => {
+    let mode = '';
+    let config = {};
+    const Twitter = function () { };
+    Twitter.prototype.stream = function (_mode, _config) {
+      mode = _mode;
+      config = _config;
+      return { on: () => { } };
+    }
+
+    twitterStream.startStream(Twitter, null, { twitter: {} }, null)(['word1', 'word2']);
+    expect(mode).to.eql('statuses/filter');
+    expect(config).to.eql({
+      filter_level: 'low',
+      language: 'en',
+      track: 'word1,word2'
+    });
+  });
+
+});
