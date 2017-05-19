777b6bc7501219fa0fa7456959b48e7258aca53f
F. Javier R. Donado <fjrd84@gmail.com>
2017-05-19 21:16:00 +0200

    Test index independently.

--- test/routes/index-spec.js
+++ test/routes/index-spec.js
@@ -2,11 +2,19 @@ const mocha = require('mocha'),
   chai = require('chai'),
   expect = chai.expect,
   chaiHttp = require('chai-http'),
-  app = require('../../app');
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  index = require('../../routes/index');
 
 chai.use(chaiHttp);
 
+// Initialize a new express app just for the test
+app = express();
+app.use(bodyParser.json());
+app.use('/', index);
+
 describe('index route', () => {
+
   it('GET / returns a JSON with a message of health-nlp-node', (done) => {
     chai.request(app)
       .get('/')
