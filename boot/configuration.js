1bcdb5cdd9587b0621bf18c5ed33d7551d732c9b
F. Javier R. Donado <fjrd84@gmail.com>
2017-09-10 13:49:49 +0200

    Add firebase configuration parameters.

--- boot/configuration.js
+++ boot/configuration.js
@@ -21,7 +21,9 @@ let configuration = {
             apiKey: process.env.FB_API_KEY,
             authDomain: process.env.FB_AUTH_DOMAIN,
             databaseURL: process.env.FB_DATABASE_URL,
-            storageBucket: process.env.FB_STORAGE_BUCKET
+            projectId: process.env.FB_PROJECT_ID,
+            storageBucket: process.env.FB_STORAGE_BUCKET,
+            messagingSenderId: process.env.FB_MESSAGING_SENDER_ID
         },
         user: {
             email: process.env.FB_EMAIL,
